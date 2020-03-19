041fd504d8fb634ef8b967c4828ba87ad6387166
xs0701 <775331818@qq.com>
2020-03-19 19:48:45 +0800

    重写支持多线程

--- req.py
+++ req.py
@@ -1,13 +1,16 @@
 import requests
-from random import choice
-IP_LIST=[]
-for line in open('''./IP_LIST.txt'''):
-    IP_LIST.append(line.strip())
+import threading
+url = ""
+headers={}
+N = 5
 
-while(1):
-    url = ''
-    proxy = {'http':choice(IP_LIST)}
-    print(proxy)
-    r = requests.get(url,proxies=proxy)
-    print(r.text)
-    input()
+def req(url):
+    re = requests.get(url,headers=headers)
+    if re.status_code != 200:
+        print(url)
+
+if __name__ == "__main__":
+    for i in range (N):
+        t = threading.Thread(target=req,args=url)
+        t.start()
+        t.join()
